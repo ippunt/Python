45294b499ec02974524aae0ff69a989d3d076f9b
Jeffrey <jeffrey.darling15@gmail.com>
2022-11-19 11:20:53 -0800

    Update ascyncio.py

--- ascyncio.py
+++ ascyncio.py
@@ -1,3 +1,9 @@
+#https://docs.python.org/3/library/asyncio.html
+"""
+asyncio is used as a foundation for multiple Python asynchronous frameworks that provide high-performance network and web-servers, database connection libraries, distributed task queues, etc.
+
+asyncio is often a perfect fit for IO-bound and high-level structured network code.
+"""
 import asyncio
 
 async def main():
